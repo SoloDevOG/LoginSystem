4aac7d928e6259eb14f310935060156d5d769ccb
Solo_Dev <80140603+SoloDevOG@users.noreply.github.com>
2021-05-30 22:21:34 -0700

    Add files via upload

---
+++ main.py
@@ -0,0 +1,25 @@
+# Code Updated By SoloDev
+
+import requests, time
+import os
+pastebin = 'https://pastebin.com/raw/jB285Hxc'
+
+username = input('Enter Your Username > ')
+os.system('cls')
+password = input('Enter Your Password > ')
+os.system('cls')
+
+response = requests.get(pastebin).text
+
+for line in response.split('\n'):
+  if username and password in line:
+    print('Correct password and username')
+    start()
+print('Invalid password or username.')
+
+# Your Code Below
+
+def start():
+  print('hello world')
+  while True:
+    input()
